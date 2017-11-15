dea243ab6dbabf9e5762983e4e712ac8701a0a0d
DarkKnightof1 <mischa@boskat.eu>
2017-11-15 13:02:36 +0100

    op 1.3

--- Js1.js
+++ Js1.js
@@ -2,3 +2,11 @@ console.log("Hello World");
 for (var teller = 0; teller < 10; teller++) {
     console.log("i heeft nu de waarde " + teller)
 }
+var mijnauto = {
+    merk: "Ford",
+    type: "Mondeo",
+    aantalWielen: "4",
+    kleur: "blauw"
+};
+
+console.log(mijnauto.kleur);
